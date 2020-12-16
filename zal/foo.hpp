2301464eaa5e108e3409e27d1667ab1492600a2b
MaverickBirdman <73669666+MaverickBirdman@users.noreply.github.com>
2020-12-16 17:08:20 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -6,11 +6,29 @@
 #include <list>
 #include <vector>
 
+using std::vector;
+using std::list;
+using std::for_each;
+using std::transform;
+
+void f1(Human& a)
+{
+	a.birthday();
+}
+
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-
-    // Twoja implementacja tutaj
-
+	transform(people.begin(), people.end(), ret_v.rbegin(), [&](Human& a){if (a.isMonster() == true){return 'n';}else{return 'y';}});
+	for_each(people.begin(), people.end(), f1);
     return ret_v;
 }
+
+
+
+
+
+
+
+
+
